a767a6d70a65021627285d4565d9060ed5de1822
Alexander Barabanov <Sasha.bar99@gmail.com>
2020-06-02 01:38:33 +0300

    crop_from_bbox implemented

--- dextr.cpp
+++ dextr.cpp
@@ -6,7 +6,7 @@
 #include <opencv2/opencv.hpp>
 #include "helpers.cpp"
 
-static int find_dextr_bit_mask(std::string image_path, std::vector<std::vector<int>> extreme_points_double_array){
+static int find_dextr_bit_mask(const std::string &image_path, std::vector<std::vector<int>> extreme_points_double_array){
     //std::string model_file = "../resnet18.pt";
     std::string model_file = "../traced_model_gpu.pt";
     //std::string model_file = "../traced_model_cpu.pt";
@@ -29,38 +29,79 @@ static int find_dextr_bit_mask(std::string image_path, std::vector<std::vector<i
 
     // Open the image file.
     cv::Mat image;
-    image = cv::imread(image_path, cv::IMREAD_COLOR);
+    //std::vector<std::vector<int>> v = {{0, 1, 2, 3}, {4, 5, 6, 7}, {8, 9, 10, 11}, {12, 13, 14, 15}};
+    //image = vecToMat(v);
+    image = cv::imread(image_path, cv::ImreadModes::IMREAD_COLOR);
     cv::Size im_size = {image.rows, image.cols};
     if (!image.data){
-        printf("No image data \n");
+        std::cout << "No image data" << std::endl;
         return -1;
     }
+    cv::Mat bgr[3];
+    cv::split(image, bgr);
 
-    // Preview image.
-    // cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE );
-    // cv::imshow("Display Image", image);
-    // cv::waitKey(0);
+    std::cout << "Image opened " << image.size << ", channels " << image.channels() << std::endl;
 
-    // Resize image to 512x512.
-    cv::Size size(512, 512);
-    cv::Mat image_resized;
-    cv::resize(image, image_resized, size);
+    // Crop image to the bounding box from the extreme points and resize
+    std::vector<int> bbox = {-22, 137, 119, 259};
+    cv::Mat crop_image = crop_from_bbox(image, bbox, true);
+    cv::Mat resize_image;
 
-    // Preview resized image.
-    // cv::namedWindow("Display Image 2", cv::WINDOW_AUTOSIZE );
-    // cv::imshow("Display Image 2", image);
-    // cv::waitKey(0);
+    cv::resize(image, resize_image, cv::Size(512, 512));
+
+    std::cout << "Image resized " << resize_image.size <<  ", channels " << resize_image.channels() << std::endl;
+
+    // Generate extreme point heat map normalized to image values
+    int minVal0 = INT_MAX;
+    int minVal1 = INT_MAX;
+    int PAD = 50;
+    for(auto & i : extreme_points_double_array) {
+        if (i[0] < minVal0){
+            minVal0 = i[0];
+        }
+        if (i[1] < minVal1){
+            minVal1 = i[1];
+        }
+    }
+
+    std::cout << "cropsize " << crop_image.size() <<  ", channels " << crop_image.channels() << std::endl;
+
+
+    std::vector<std::vector<int>> extreme_points = extreme_points_double_array;
+    for(int i = 0; i < extreme_points.size(); i++){
+        std::cout << "extreme_points[" << i << "]" << extreme_points[i][0] << ", " << extreme_points[i][1] << std::endl;
+        //extreme_points[i][0] = 512 * (extreme_points[i][0] - minVal0 + PAD) * [1 / crop_image.size(), 1 / crop_image.size()[0]];
+        //extreme_points[i][1] = extreme_points[i][1] - minVal1 + PAD;
+        // (512 * extreme_points * [1 / crop_image.shape[1], 1 / crop_image.shape[0]])
+        std::cout << "extreme_points[" << i << "]" << extreme_points[i][0] << ", " << extreme_points[i][1] << std::endl;
+        std::cout << std::endl;
+    }
+
+    std::cout << "Extreme points ok" << std::endl;
+
+    std::vector<std::vector<float>> extreme_heatmap;
+    extreme_heatmap = make_gt(resize_image, extreme_points, 10);
+    //extreme_heatmap = helpers.cstm_normalize(extreme_heatmap, 255)
+
+    std::cout << "Extreme heatmap ok " << extreme_heatmap.size() << std::endl;
 
     // Open image as tensor.
-    std::vector<int64_t> sizes = {1, 4, 512, 512};
-    at::TensorOptions options(at::ScalarType::Byte);
-    at::Tensor tensor_image = torch::from_blob(image.data, at::IntList(sizes), options);
-    tensor_image = tensor_image.toType(at::kFloat);
+    //at::IntArrayRef sizes = {1, 4, 512, 512};
+    at::Tensor tensor_image = matToTensor(resize_image);
+
+    //std::vector<int64_t> sizes = {1, 4, 512, 512};
+    //at::TensorOptions options(at::ScalarType::Byte);
+    //at::Tensor tensor_image = torch::from_blob(image.data, at::IntList(sizes), options);
+    //tensor_image = tensor_image.toType(at::kFloat);
+
+    std::cout << "Image to tensor " << tensor_image.sizes() << std::endl;
 
     //Create a vector of inputs.
     std::vector<torch::jit::IValue> inputs;
     //inputs.push_back(torch::ones({1, 4, 512, 512}).to(device));
-    inputs.push_back(tensor_image.to(device));
+    inputs.emplace_back(tensor_image.to(device));
+
+    std::cout << "Tensor to gpu" << std::endl;
 
     // Execute the model and turn its output into a tensor.
     std::cout << "Performing forward pass" << std::endl;
@@ -72,16 +113,18 @@ static int find_dextr_bit_mask(std::string image_path, std::vector<std::vector<i
     pred = 1 / (1 + exp(-pred));
     pred = at::squeeze(pred);
 
+    cv::namedWindow("pred image", cv::WINDOW_AUTOSIZE );
+    cv::imshow("pred image", tensorToMat(pred));
+    cv::waitKey(0);
+
     std::cout << "pred sizes" << pred.sizes() << std::endl;
 
-    std::vector<int> bbox = {-22, 137, 119, 259};
+    at::Tensor bit_mask_array = crop2fullmask(pred, bbox, im_size);
 
-    std::vector<std::vector<int>> bit_mask_array = crop2fullmask(pred, bbox, im_size);
+    std::cout << "bit_mask_array sizes " << bit_mask_array.sizes() << std::endl;
 
-    std::cout << "bit_mask_array sizes " << bit_mask_array.size() << " " << bit_mask_array[0].size() << std::endl;
-
-    cv::Mat bit_mask_image = make_masks_image(bit_mask_array);
-
+    cv::Mat bit_mask_image = tensorToMat(bit_mask_array);
+    cv::imwrite("../images/dextr-results/output.jpg", bit_mask_image);
     //cv::namedWindow("Display Image 3", cv::WINDOW_AUTOSIZE );
     //cv::imshow("Display Image 3", mask_image);
     //cv::waitKey(0);
--- helpers.cpp
+++ helpers.cpp
@@ -20,9 +20,18 @@ static  cv::Mat vecToMat(const std::vector<std::vector<int>> &vecIn) {
 
 static cv::Mat tensorToMat(at::Tensor &tensor) {
     auto sizes = tensor.sizes();
-    cv::Mat resultImg(sizes[0], sizes[1], 0);
-    std::memcpy((void*)resultImg.data, tensor.data_ptr(), sizeof(torch::kU8)*tensor.numel());
+    cv::Mat resultImg(sizes[0], sizes[1], CV_8UC1);
+    std::memcpy((void*)resultImg.data, tensor.data_ptr(), sizeof(torch::kUInt8)*tensor.numel());
+
     return resultImg;
+}
+
+static at::Tensor matToTensor(cv::Mat &mat) {
+    at::TensorOptions options(at::ScalarType::Byte);
+    at::Tensor result = torch::from_blob(mat.data, {mat.cols, mat.rows} , options);
+    result = result.toType(at::ScalarType::Float);
+
+    return result;
 }
 
 static std::vector<std::vector<int>> matToArray(cv::Mat &mat) {
@@ -30,10 +39,11 @@ static std::vector<std::vector<int>> matToArray(cv::Mat &mat) {
     for (int i = 0; i < mat.rows; i++){
         result.insert(result.end(), mat.ptr<uchar>(i), mat.ptr<uchar>(i)+mat.cols);
     }
+
     return result;
 }
 
-static std::vector<std::vector<int>> crop2fullmask(at::Tensor crop_mask, std::vector<int> bbox, cv::Size &im_size){
+static at::Tensor crop2fullmask(at::Tensor crop_mask, std::vector<int> bbox, cv::Size &im_size){
     // Borders of image
     std::vector<int> bounds = {0, 0, im_size.height - 1, im_size.width - 1};
 
@@ -54,27 +64,87 @@ static std::vector<std::vector<int>> crop2fullmask(at::Tensor crop_mask, std::ve
 
     cv::Mat crop_mask_Mat = tensorToMat(crop_mask);
 
-    cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE );
-    cv::imshow("Display Image", crop_mask_Mat);
-    cv::waitKey(0);
+    //cv::namedWindow("Display Image", cv::WINDOW_AUTOSIZE );
+    //cv::imshow("Display Image", crop_mask_Mat);
+    //cv::waitKey(0);
 
     cv::resize(crop_mask_Mat, crop_mask_Mat, {bbox[2] - bbox[0] + 1, bbox[3] - bbox[1] + 1}, cv::INTER_CUBIC);
 
-    cv::namedWindow("Display Image 2", cv::WINDOW_AUTOSIZE );
-    cv::imshow("Display Image 2", crop_mask_Mat);
-    cv::waitKey(0);
+    //std::vector<std::vector<int>> crop_mask_Arr = matToArray(crop_mask_Mat);
 
-    std::vector<std::vector<int>> crop_mask_Arr = matToArray(crop_mask_Mat);
-
-    std::cout << "crop_mask_Arr " << crop_mask_Arr.size() << std::endl;
+    std::cout << "crop_mask_Arr " << crop_mask.sizes() << std::endl;
 
     std::vector<int> inds = {22, 0, 141, 122};
-    std::vector<std::vector<int>> result(im_size.width, std::vector<int>(im_size.height));
+    //std::vector<std::vector<int>> result(im_size.width, std::vector<int>(im_size.height));
+    at::Tensor result = torch::zeros(torch::IntArrayRef {im_size.width, im_size.height});
+
+    //TODO fix following part
     for(int i = 0; i < 2; i++) {
-        result[bbox_valid[i + 1] + 1, bbox_valid[i] + 1] = crop_mask_Arr[inds[i + 1] + 1, inds[i] + 1];
+        result[bbox_valid[i + 1] + 1][bbox_valid[i] + 1] = crop_mask[inds[i + 1] + 1][inds[i] + 1];
     }
 
     return result;
+}
+
+static cv::Mat crop_from_bbox(cv::Mat &img, std::vector<int> &bbox, bool zero_pad=false){
+    // Borders of image
+    std::vector<int> bounds = {0, 0, img.rows - 1, img.cols - 1};
+    std::cout << "crop_from_bbox bounds " << bounds << std::endl;
+
+    // Valid bounding box locations as (x_min, y_min, x_max, y_max)
+    std::vector<int> bbox_valid = {std::max(bbox[0], bounds[0]),
+                                   std::max(bbox[1], bounds[1]),
+                                   std::min(bbox[2], bounds[2]),
+                                   std::min(bbox[3], bounds[3])};
+
+    std::cout << "bbox_valid " << bbox_valid << std::endl;
+    cv::Mat crop;
+    at::Tensor crop_tensor;
+    at::Tensor img_tensor = matToTensor(img);
+    std::cout << "img_tensor sizes " << img_tensor.sizes() << std::endl;
+    if(zero_pad){
+        // Initialize crop size (first 2 dimensions)
+        crop_tensor = torch::zeros(torch::IntArrayRef {bbox[3] - bbox[1] + 1, bbox[2] - bbox[0] + 1});
+        std::cout << "crop_tensor sizes " << crop_tensor.sizes() << std::endl;
+
+        // Offsets for x and y
+        std::vector<int> offsets = {-bbox[0], -bbox[1]};
+        std::cout << "offsets " << offsets << std::endl;
+    } else {
+        assert (bbox == bbox_valid);
+        crop_tensor = torch::zeros(torch::IntArrayRef {bbox_valid[3] - bbox_valid[1] + 1, bbox_valid[2] - bbox_valid[0] + 1});
+    }
+
+    //TODO add inds calculation
+    std::vector<int> inds = {22, 0, 141, 122};
+    // bbox_valid 0 137 119 259
+
+    std::cout << "crop_from_bbox crop_tensor sizes " << crop_tensor.sizes() << std::endl;
+    std::cout << "crop_from_bbox img_tensor sizes " << img_tensor.sizes()<< ", img_tensor dims " << img_tensor.ndimension() << std::endl;
+
+    crop_tensor.squeeze();
+    int height = inds[3] - inds[1];
+    int width = inds[2] - inds[0];
+
+    std::cout << "crop_tensor height = " << height << std::endl;
+    std::cout << "crop_tensor width = " << width << std::endl;
+
+    for(int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            //std::cout << "crop_tensor[" << i << "][" << j << "] = " << crop_tensor[i][j] << std::endl;
+            //std::cout << "img_tensor[" << i + bbox_valid[1] << "][" << j + bbox_valid[0] << "] = " << img_tensor[i + bbox_valid[1]][j + bbox_valid[0]] << std::endl;
+            crop_tensor[i + inds[1]][j + inds[0]] = img_tensor[i + bbox_valid[1]][j + bbox_valid[0]];
+        }
+    }
+
+    std::cout << "tensorToMat..."<< std::endl;
+    crop = tensorToMat(crop_tensor);
+
+    cv::namedWindow("Display crop", cv::WINDOW_AUTOSIZE );
+    cv::imshow("Display crop", crop);
+    cv::waitKey(0);
+
+    return crop;
 }
 
 static cv::Mat make_masks_image(std::vector<std::vector<int>> &results) {
@@ -85,3 +155,77 @@ static cv::Mat make_masks_image(std::vector<std::vector<int>> &results) {
     return mask_image;
 }
 
+static std::vector<std::vector<float>> make_gaussian(const std::vector<int> &size, const std::vector<int> &center, int sigma=10){
+    std::vector<std::vector<int>> gaussian;
+    std::vector<float> x;
+    std::vector<float> y;
+    float x0;
+    float y0;
+    x.reserve(size[1]);
+    y.reserve(size[0]);
+    for(int i = 0; i < size[1]; i++) {
+        x.push_back(i);
+    }
+    for(int i = 0; i < size[0]; i++) {
+        y.push_back(i);
+    }
+
+    if(center.empty()){
+        x0 = y0 = size[0]/2;
+    } else {
+        y0 = center[0];
+        x0 = center[1];
+    }
+
+    std::cout << "gauss x0, y0 " << x0 << " " << y0 << std::endl;
+
+    std::vector<float> linear_x_x0 = x;
+    for(int i = 0; i < size[1]; i++) {
+        linear_x_x0[i] = pow(linear_x_x0[i] - x0, 2);
+    }
+    std::vector<float> linear_y_y0 = y;
+    for(int i = 0; i < size[0]; i++) {
+        linear_y_y0[i] = pow(linear_y_y0[i] - y0, 2);
+    }
+
+    std::vector<std::vector<float>> gauss(size[1], std::vector<float>(size[0], 0));
+    for(int i = 0; i < size[1]; i++) {
+        for(int j = 0; j < size[0]; j++) {
+            gauss[i][j] = exp((-4 * log(2) * (linear_x_x0[i] + linear_y_y0[j])/pow(sigma, 2)));
+
+            if (i == 0 && j == 0 || i == 50 && j == 50 || i == 180 && j == 283 || i == 511 && j == 511){
+                std::cout << "gauss" << i << j << " " << gauss[i][j] << std::endl;
+            }
+        }
+    }
+
+    return gauss;
+};
+
+static std::vector<std::vector<float>> make_gt(const cv::Mat &img, const std::vector<std::vector<int>> &lables, int sigma=10){
+    std::vector<std::vector<float>> gt;
+    int h = img.rows;
+    int w = img.cols;
+    std::cout << "make_gt: height " << h << ", width " << w << std::endl;
+    std::cout << "make_gt: lables size " << lables.size() << std::endl;
+    for (int i = 0; i < lables.size(); i++) {
+        std::cout << "lables[" << i << "]" << lables[i][0] << ", " << lables[i][1] << std::endl;
+    }
+
+    if(lables.empty()){
+        std::cout << "make_gt: lables.empty()" << std::endl;
+        gt = make_gaussian({h, w}, {h / 2, w / 2}, sigma=sigma);
+    } else {
+        if (lables.size() == 1) {
+            // labels = labels[np.newaxis]
+        }
+        //gt.emplace_back(torch::zeros({h, w}));
+        for (const auto & lable : lables) {
+            gt = MAX(gt, make_gaussian({h, w}, {lable[0], lable[1]}, sigma));
+            std::cout << "make_gt: make_gaussian with size " << h << ", " << w << " and center in " << lable[0] << ", " << lable[1] << std::endl;
+
+        }
+    }
+
+    return gt;
+}
